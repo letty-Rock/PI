5a9d065d95e28896270a7a3d0ac0569266128bb4
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-09-06 19:43:41 -0300

    Adote
    imagens

--- src/pages/Adotar/adotar.tsx
+++ src/pages/Adotar/adotar.tsx
@@ -1,11 +1,125 @@
 import React from "react";
+import {
+ Adote,
+ Background,
+ Box,
+ Filtro,
+ Grade,
+ AD1,
+ AD2,
+ AD3,
+ AD4,
+ Imagem1,
+ Imagem2,
+ Imagem3,
+ Imagem4,
+} from "./styles";
 
 const Adotar = () => {
   return (
-    <div>
-      <h1>Quero Adotar!</h1>
-    </div>
+    
+    <Adote>
+      <Background>
+        <Box >
+          <h1>QUERO ADOTAR!</h1>
+          <h1>Conheça os Aumigos que estão precisando de um lar!</h1>
+        </Box>
+        </Background>
+
+
+      
+
+    <Filtro>
+        <h1>
+          Filtro
+        </h1>
+        <h1>
+          Porte
+        </h1>
+
+        <p><input type="radio" name="Porte" />Pequeno</p>
+        <p><input type="radio" name="Porte" />Médio</p>
+        <p><input type="radio" name="Porte" />Grande</p>
+
+        <h1>
+          Idade
+        </h1>
+
+        <p><input type="radio" name="Idade" />0 a 2 anos</p>
+        <p><input type="radio" name="Idade" />3 a 5 anos</p>
+        <p><input type="radio" name="Idade" />6 a 10 anos</p>
+        <p><input type="radio" name="Idade" />+ 10 anos</p>
+
+        <h1>
+          Sexo
+        </h1>
+
+        <p><input type="radio" name="Sexo" />Macho</p>
+        <p><input type="radio" name="Sexo" />Fêmea</p>
+
+        <h1>
+          Espécie
+        </h1>
+
+        <p><input type="radio" name="Especie" />Cachorro</p>
+        <p><input type="radio" name="Especie" />Gato</p>
+
+        <input type="button" value="Filtrar"/>
+
+      </Filtro>
+      <Grade>
+      <AD1>
+        <Imagem1/>
+      <h1>  MEL </h1>
+       <p> &bull; Idade: 2 Anos</p>
+       <p> &bull; Porte: N/A</p>
+       <p> &bull; Vacinado:Sim</p>
+       <p> &bull; Sexo:Fêmea</p>
+       <p> &bull; Espécie:Gato</p>
+       
+      </AD1>
+      <AD2>
+        <Imagem2/>
+      <h1>  MEL </h1>
+       <p> &bull; Idade: 2 Anos</p>
+       <p> &bull; Porte: N/A</p>
+       <p> &bull; Vacinado:Sim</p>
+       <p> &bull; Sexo:Fêmea</p>
+       <p> &bull; Espécie:Gato</p>
+       
+      
+      
+      </AD2>
+      <AD3>
+        <Imagem3/>
+      
+      <h1>  MEL </h1>
+       <p> &bull; Idade: 2 Anos</p>
+       <p> &bull; Porte: N/A</p>
+       <p> &bull; Vacinado:Sim</p>
+       <p> &bull; Sexo:Fêmea</p>
+       <p> &bull; Espécie:Gato</p>
+       
+      
+      
+      </AD3>
+      <AD4>
+        <Imagem4/>
+      <h1>  MEL </h1>
+       <p> &bull; Idade: 2 Anos</p>
+       <p> &bull; Porte: N/A</p>
+       <p> &bull; Vacinado:Sim</p>
+       <p> &bull; Sexo:Fêmea</p>
+       <p> &bull; Espécie:Gato</p>
+       
+      
+      
+      </AD4>
+      </Grade>
+      </Adote>
+      
   );
 };
+
 
 export default Adotar;
--- src/pages/Adotar/styles.ts
+++ src/pages/Adotar/styles.ts
@@ -1,5 +1,40 @@
 import styled from "styled-components";
+import Imagem from "../../assets/images/Gato_adotar.png";
 
-const Container = styled.input``;
 
-export {};
+const Adote = styled.div``;
+const Background = styled.div`
+background-image: url(${Imagem});
+    height: 100%;
+  width: 100%;
+  display: flex;
+`;
+const Box = styled.div``;
+const Filtro = styled.div``;
+const Grade = styled.div``;
+const AD2 = styled.div``;
+const AD3 = styled.div``;
+const AD4 = styled.div``;
+const Imagem1 = styled.div``;
+const Imagem2 = styled.div``;
+const Imagem3 = styled.div``;
+const Imagem4 = styled.div``;
+const AD1 = styled.div``;
+
+
+
+export {
+    Adote,
+ Background,
+ Box,
+ Filtro,
+ Grade,
+ AD1,
+ AD2,
+ AD3,
+ AD4,
+ Imagem1,
+ Imagem2,
+ Imagem3,
+ Imagem4,
+};
